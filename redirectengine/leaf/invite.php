a2f721f7882e8a23c6c4fd832fcf6360580359c4
alexanderanton <79249947+alexanderanton@users.noreply.github.com>
2021-05-11 13:12:43 +0300

    Add files via upload

---
+++ redirectengine/leaf/invite.php
@@ -0,0 +1,13 @@
+<php
+header("Location: "https://discord.com/api/oauth2/authorize?client_id=836970510528020531&permissions=8&scope=bot");
+die();
+>
+
+<html>
+   <body>
+       <center>
+       If the redirect isnt working, click <a href="">here</a>. <br> <br>
+       Powered by asdredirectengine
+       </center>
+   </body>
+</html>
