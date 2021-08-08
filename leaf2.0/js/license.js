015fba800d1e03b2ab10c9c4c560c111ac8ced46
Alexander Anton <79249947+alexanderanton@users.noreply.github.com>
2021-08-08 22:19:08 +0300

    LEAF 2.0 YOOOOO
    LeafBOT 2.0 is on the way to you Discord Server!
    Release date: 23th of October 2021!
    -----------------------------------------------------
    At the release date of Leaf2.0, the code for LeafBETA will turn public!

---
+++ leaf2.0/js/license.js
@@ -0,0 +1,9 @@
+const data = { 
+    "text": "(c) 2021 Alexandru Anton. Leaf2.0 aka LeafBOT2.0 is not an open-source software, so you can't copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software." 
+}
+
+document.getElementById("text").innerText += data.text;
+
+function back(){
+    history.back();
+}
